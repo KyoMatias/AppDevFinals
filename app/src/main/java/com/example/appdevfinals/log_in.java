71eb81a689f830dec6e5f85cefefa761d33c1365
CIIT601PC05\Student <it@ciit.edu.ph>
2024-07-01 15:37:29 +0800

    added
    main menu
    register
    
    workign on partial functioanlity of login

--- app/src/main/java/com/example/appdevfinals/log_in.java
+++ app/src/main/java/com/example/appdevfinals/log_in.java
@@ -1,6 +1,9 @@
 package com.example.appdevfinals;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.Button;
 public class log_in extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button loginBtn;
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_in);
@@ -20,7 +26,69 @@ public class log_in extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        /*Login functionality
+        input user credentials
+        click login button
+        check if credentials are correct in local text file which will be stored in the raw/assets folder
+        if credentials are correct, go to main menu
+        if credentials are incorrect, after 10 tries, disable login button
+        */
+
+
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                Intent open_main_menu = new Intent(log_in.this, main_menu.class);
+                startActivity(open_main_menu);
+
+                }
+        });
     }
 
+/*        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        b1 = (Button)findViewById(R.id.button);
+        ed1 = (EditText)findViewById(R.id.editText);
+        ed2 = (EditText)findViewById(R.id.editText2);
+
+        b2 = (Button)findViewById(R.id.button2);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong
+                            Credentials",Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        }
+    }*/
 }
